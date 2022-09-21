9243397d4e8b92011268df5948db7a0951c6bea1
Binh Bui <binhbv@gmail.com>
2022-09-21 11:50:43 +0700

    add jest to UT test

---
+++ __tests__/test.ts
@@ -0,0 +1,37 @@
+// This file use the Jest framework to test the code in the file
+// https://jestjs.io/docs/getting-started
+
+import { generateDescriptionFromDom } from '../src/index';
+
+import cheerio from 'cheerio';
+import axios from 'axios';
+
+const getHtml = async (url: string) => {
+  const { data } = await axios.get(url);
+  return data;
+};
+
+const cheerioInit = async (url: string) => {
+  console.log('url', url);
+  const html = await getHtml(url);
+  return cheerio.load(html);
+};
+
+const item = {};
+
+describe('generateDescriptionFromDom', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should return a object', async () => {
+    const $: any = await cheerioInit(
+      'https://www.city.fukuoka.lg.jp/nosui/seisakukikaku/umakamon-award2022.html'
+    );
+
+    // This is a sample article
+    const item: any = {};
+    const result = generateDescriptionFromDom($, item);
+    expect(typeof result).toBe('object');
+  });
+});
--- test/test.mjs
+++
@@ -1,9 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { generateDescriptionFromDom } from '../dist/index.modern.mjs';
-
-// Node 18+ support UT test, does need mocha or jest anymore.
-// Read more: https://nodejs.org/api/test.html
-test('generateDescriptionFromDom', (_t) => {
-  assert.equal(true, true);
-});
